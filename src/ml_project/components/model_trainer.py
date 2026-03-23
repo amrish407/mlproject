86425344d378e3dfac5b29286f11a9ca464aa30f
unknown <amrishyadav407@gmail.com>
2026-03-24 02:42:17 +0530

    dagshub and mlflow working

--- src/ml_project/components/model_trainer.py
+++ src/ml_project/components/model_trainer.py
@@ -2,6 +2,9 @@ import os
 import sys
 from dataclasses import dataclass
 import numpy as np
+import mlflow
+import dagshub
+from urllib.parse import urlparse
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
@@ -102,6 +105,45 @@ class ModelTrainer:
 
             print("This is the best model:")
             print(best_model_name)
+
+            model_names = list(models.keys())
+
+            actual_model = ""
+
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = model
+
+            best_params = params[actual_model]
+
+            dagshub.init(repo_owner='amrish407', repo_name='mlproject', mlflow=True)
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            #mlflow
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+                
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
